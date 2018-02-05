8045a0b3a994d7687fded36b9b96539e7572c09b
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2018-02-05 22:06:55 +0000

    Fixed navigation button position and transitioning on click.

--- static/js/site.js
+++ static/js/site.js
@@ -1,9 +1,10 @@
-/* Open when someone clicks on the span element */
 function openNav() {
+    $(".bar").css("background-color", "transparent");
     document.getElementById("myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
+    $(".bar").css("background-color", "#ffffff");
     document.getElementById("myNav").style.width = "0%";
 }

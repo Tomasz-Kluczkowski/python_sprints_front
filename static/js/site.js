e8ab8b6aa0b8349c30aa92486823eab9a3a32df7
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2018-02-06 14:35:55 +0000

    Title section finished. Mission section finished.

--- static/js/site.js
+++ static/js/site.js
@@ -1,10 +1,24 @@
+var buttonOpenContainer = $(".navigation__button-open-container");
+var hamburgerBar = $(".navigation__hamburger-bar");
+var navigationOverlay = $(".navigation__overlay");
+var buttonCloseContainer = $(".navigation__button-close-container");
+
 function openNav() {
-    $(".full-screen-nav__bar").css("background-color", "transparent");
-    $(".full-screen-nav__overlay").css("width", "100%");
+    buttonOpenContainer.css("pointer-events", "none");
+    buttonOpenContainer.toggle(500, showCloseButton);
+    hamburgerBar.css("background-color", "transparent");
+    navigationOverlay.css("width", "100%");
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    $(".full-screen-nav__bar").css("background-color", "#ffffff");
-    $(".full-screen-nav__overlay").css("width", "0%");
+    hamburgerBar.css("background-color", "#ffffff");
+    navigationOverlay.css("width", "0%");
+    buttonOpenContainer.css("display", "inline");
+    buttonOpenContainer.css("pointer-events", "auto");
+    buttonCloseContainer.css("display", "none");
+}
+
+function showCloseButton() {
+    buttonCloseContainer.css("display", "inline");
 }

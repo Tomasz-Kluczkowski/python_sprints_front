4a21ff2f76270afe8b8c58802250d756b1ef39e2
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2018-02-07 22:53:08 +0000

    Implemented background image replacement depending on the screen size.

--- static/js/site.js
+++ static/js/site.js
@@ -22,3 +22,38 @@ function closeNav() {
 function showCloseButton() {
     buttonCloseContainer.css("display", "inline");
 }
+
+
+// Responsive background image constructor.
+class ResponsiveBackgroundImage {
+
+    constructor(element) {
+        this.element = element;
+        this.img = element.querySelector('.responsive');
+        this.src = '';
+
+        this.img.addEventListener('load', () => {
+            this.update();
+        });
+
+        if (this.img.complete) {
+            this.update();
+        }
+    }
+
+    update() {
+        let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
+        if (this.src !== src) {
+            this.src = src;
+            this.element.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url("' + this.src + '")';
+        }
+    }
+}
+
+// Create responsive background images.
+let elements = document.querySelectorAll('[data-image-type="responsive"]');
+for (let i=0; i<elements.length; i++) {
+    new ResponsiveBackgroundImage(elements[i]);
+}
+
+
